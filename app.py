6828afeb15bf0fda2366a88a7d578afd780fdc5a
Charles Pehlivanian <pehlivaniancharles@gmail.com>
2025-07-18 07:06:10 -0400

    Version 1.0

--- app.py
+++ app.py
@@ -47,6 +47,4 @@ def properties():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=9002, debug=False)
